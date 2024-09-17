dfad1e1aeab2416a19cf3ab15e2bfa3aeba5087d
jan preisler <gamingacaumd@gmail.com>
2024-09-17 09:32:49 +0200

    Stage

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,15 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿//Dotaznik
+using System.Net.Mail;
+
+Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+
+
+Console.WriteLine("Aké je tvoje oblubené jedlo");
+string? jedlo = Console.ReadLine();
+Console.WriteLine("Ahoj volám sa " + meno + " Mám " + vek + " a moje oblubené jedlo je " + jedlo + "." + "\n");
+// Koniec dotaznika
