7fefc0e83813a0a7c9d918d91f2f9ee5e816e721
jan preisler <gamingacaumd@gmail.com>
2024-09-17 09:35:00 +0200

    dotazník

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -2,7 +2,7 @@
 bool isAdult = false;
 float accountBalance = 150.72f;
 
-
+//vytvaranie premenych a testovanie
 Console.WriteLine(znak);
 Console.WriteLine(accountBalance);
 Console.WriteLine(isAdult);
