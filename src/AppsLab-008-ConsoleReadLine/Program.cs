7a885740e344b6d45e27a4f956ecfbea2833a175
jan preisler <gamingacaumd@gmail.com>
2024-09-17 11:10:06 +0200

    Push

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,15 +1,59 @@
-﻿//Dotaznik
-using System.Net.Mail;
+﻿
+/*Console.WriteLine(25 == 30);
+Console.WriteLine(50 == 50);
+Console.WriteLine(25 != 20);
+Console.WriteLine(30 != 30);
+Console.WriteLine(67 > 3);
+Console.WriteLine(5 > 35);
+Console.WriteLine(1 < 99);
+Console.WriteLine(99 < 1);
+Console.WriteLine(5 >= 3);
+Console.WriteLine(10 >= 369);
+Console.WriteLine(5 <= 5);
+Console.WriteLine(98 <= 5);*/
 
-Console.WriteLine("Ako sa voláš?");
-string? meno = Console.ReadLine();
 
-Console.WriteLine("Koľko máš rokov?");
-string vstup = Console.ReadLine() ?? "0";
-int vek = int.Parse(vstup);
+//zadanie mena
+var menouzivatela = "Janko";
+var heslouzivatela = 123;
+
+//znova zadanie
+var zadaniemena = "Janko";
+var zadaniehesla = 123;
+
+/*Console.WriteLine("Heslo správne " +  porovnaniehesiel);
+Console.WriteLine("Meno správne " + porovnavaniemena);
+Console.WriteLine("Pustime pouzivatela " + prijPouzivatela);*/
+
+if (menouzivatela == zadaniemena && heslouzivatela == zadaniehesla)
+{
+    Console.WriteLine("Všetko sa zhoduje Vitaj " + menouzivatela);
 
 
-Console.WriteLine("Aké je tvoje oblubené jedlo");
-string? jedlo = Console.ReadLine();
-Console.WriteLine("Ahoj volám sa " + meno + " Mám " + vek + " a moje oblubené jedlo je " + jedlo + "." + "\n");
-// Koniec dotaznika
+
+}
+else
+{
+    Console.WriteLine("Nesprávne meno alebo heslo ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
