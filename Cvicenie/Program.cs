6d978a76219bed733e63bfb418b91edc640d68c6
jan preisler <gamingacaumd@gmail.com>
2024-10-08 11:06:50 +0200

    commith

---
+++ Cvicenie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)    
+        {
+            Console.WriteLine("Zadaj text:");
+            string text = Console.ReadLine();
+
+            int pocetsamohlasok = PocetSamohlasok(text);
+
+            if (pocetsamohlasok <= 4)
+                Console.WriteLine("Text obsahuje " + pocetsamohlasok + " samohlásky.");
+            else
+                Console.WriteLine("Text obsahuje " + pocetsamohlasok + " samohlások.");
+        }
+        static int PocetSamohlasok(string text)
+        {
+            char[] samohlasky = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            int pocet = 0;
+
+            foreach (char pismeno in text)
+            {
+                if (Array.Exists(samohlasky, samohlaska => samohlaska == pismeno))
+                {
+                    pocet++;
+                }
+            }
+            return pocet;
+        }
+    }
+}
---
+++ ulohy/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        /*int vek = 16 ;
+        string meno = "Jan" ;
+        bool isstudent = true ;
+        double vyska = 1.70 ;
+
+        Console.WriteLine("Moj vek je "+vek +" rokov"+"\n"  + "Moje meno je "+meno + "\n" + 
+            "Som student "+isstudent + "\n" + "Moja vyska je "+ vyska + " metra"+ "\n");*/
+
+        /*Console.WriteLine("Napis vek");
+        var vek = Console.ReadLine();
+        int vekdva = int.Parse(vek);
+
+        if (vekdva >= 18)
+        {
+            Console.WriteLine("Si dospely");
+        }
+        else
+        {                                                                                            
+            Console.WriteLine("Nie si dospely");
+        }*/
+
+        /* Random rnd = new Random();
+         int vygeneradovane = rnd.Next(maxValue:100);
+         Console.WriteLine("Napis cislo");
+         string hadame = Console.ReadLine() ?? "0"; 
+         int hademedva = int.Parse(hadame);*/
+
+        
+        Random rnd = new Random();
+        int vygeneradovane = rnd.Next(maxValue: 100);
+        int pokus = 0;
+        int tip = 0;
+
+        
+
+        while (tip!= vygeneradovane)
+        {
+            Console.WriteLine("Napis cislo");
+            tip = int.Parse(Console.ReadLine());
+
+            pokus++;
+            
+
+            if (tip > vygeneradovane)
+            {
+                Console.WriteLine("Cislo je velke");
+            }
+            else if ( tip < vygeneradovane)
+            {
+                Console.WriteLine("Cislo je male");
+            }
+            else if (tip == vygeneradovane) 
+            {
+                Console.WriteLine("Gratulujem uhadol si cislo." + " Mal si " + pokus +" pokusov.");
+                break;
+            }
+            
+           
+        }
+
+    }
+
+}
+
+
+
